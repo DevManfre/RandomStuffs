cd0871113b35293e552a68e3005dbd144f606d60
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2023-03-07 19:04:49 +0100

    :poop: Shit collatz code

---
+++ Python/CollatzConjecture/CollatzConjecture.py
@@ -0,0 +1,104 @@
+import time
+
+class CollatzConjecture:
+    def __init__ (self, *args):
+        # Case: 1 int param
+        if len(args) == 1 and type(args[0]) is int:
+            self.n = args[0]
+            start = time.time()
+            self.sequence = CollatzConjecture.__getSequence(self.n)
+            end = time.time()
+            self.elapsedTime = end - start
+        # Case: 2 int param
+        elif len(args) == 2 and type(args[0]) is type(args[1]) is int:
+            start = time.time()
+            self.sequence = {x : CollatzConjecture.__getSequence(x) for x in range(args[0], args[1])}
+            end = time.time()
+            self.elapsedTime = end - start
+        # Case: 1 list or tuple param
+        elif len(args) ==1 and type(args[0]) is tuple or type(args[0]) is list:
+            start = time.time()
+            self.sequence = {x : CollatzConjecture.__getSequence(x) for x in range(args[0][0], args[0][1])}
+            end = time.time()
+            self.elapsedTime = end - start
+
+    def __getSequence(n : int) -> list:
+        if n == 1:
+            return [1]
+        elif CollatzConjecture.__isPair(n):
+            return [n] + CollatzConjecture.__getSequence(int(n/2))
+        return [n] + CollatzConjecture.__getSequence(int(3*n+1))
+    
+    def __isPair(n : int) -> bool:
+        if n % 2 == 0:
+            return True
+        return False
+    
+    def __str__(self) -> str:
+        try:
+            type(self.n)
+            return f"{self.n} -> {str(self.sequence)}"
+        except Exception:
+            string = ""
+            for k in self.sequence.keys():
+                string += f"{k} -> {self.sequence[k]}\n"
+            return string[:len(string)-1]
+
+    def fullDescription(self):
+        return f"{self.__str__()}\nElapsed time = {self.elapsedTime}"
+
+class CollatzConjecture_v2:
+    def __init__ (self, *args):
+        # TODO: Possible optimization through BinaryTree
+
+        # Case: 1 int param
+        if len(args) == 1 and type(args[0]) is int:
+            self.n = args[0]
+            start = time.time()
+            self.sequence = CollatzConjecture_v2.__getSequence(self.n)
+            end = time.time()
+            self.elapsedTime = end - start
+        # Case: 2 int param
+        elif len(args) == 2 and type(args[0]) is type(args[1]) is int:
+            start = time.time()
+            self.sequence = {x : CollatzConjecture_v2.__getSequence(x) for x in range(args[0], args[1])}
+            end = time.time()
+            self.elapsedTime = end - start
+        # Case: 1 list or tuple param
+        elif len(args) ==1 and type(args[0]) is tuple or type(args[0]) is list:
+            start = time.time()
+            self.sequence = {x : CollatzConjecture_v2.__getSequence(x) for x in range(args[0][0], args[0][1])}
+            end = time.time()
+            self.elapsedTime = end - start
+
+    def __getSequence(n : int) -> list:
+        if n == 1:
+            return [1]
+        elif CollatzConjecture_v2.__isPair(n):
+            return [n] + CollatzConjecture_v2.__getSequence(int(n/2))
+        return [n] + CollatzConjecture_v2.__getSequence(int(3*n+1))
+    
+    def __isPair(n : int) -> bool:
+        if n % 2 == 0:
+            return True
+        return False
+    
+    def __str__(self) -> str:
+        try:
+            type(self.n)
+            return f"{self.n} -> {str(self.sequence)}"
+        except Exception:
+            string = ""
+            for k in self.sequence.keys():
+                string += f"{k} -> {self.sequence[k]}\n"
+            return string[:len(string)-1]
+
+    def fullDescription(self):
+        return f"{self.__str__()}\nElapsed time = {self.elapsedTime}"
+
+A = CollatzConjecture(1, 15001)
+B = CollatzConjecture_v2(1, 15001)
+print(A.elapsedTime)
+print(B.elapsedTime)
+# Di Base la percentuale di differenza è di massimo 1%
+print(str(round((A.elapsedTime-B.elapsedTime)/(A.elapsedTime+B.elapsedTime)*100, 3)) + "%")
