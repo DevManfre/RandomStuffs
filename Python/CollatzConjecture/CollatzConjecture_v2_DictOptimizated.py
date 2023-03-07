29cb50a79fe941df2f058636703bd82b186e4188
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2023-03-08 00:04:28 +0100

    🏗️ Create more module

--- Python/CollatzConjecture/CollatzConjecture_v2_DictOptimizated.py
+++ Python/CollatzConjecture/CollatzConjecture_v2_DictOptimizated.py
@@ -1,33 +1,46 @@
 import time
 
-class CollatzConjecture:
+class CollatzConjecture_v2_DictOptimizated:
     def __init__ (self, *args):
+        self.optimization = {1:[1]}
+
         # Case: 1 int param
         if len(args) == 1 and type(args[0]) is int:
             self.n = args[0]
             start = time.time()
-            self.sequence = CollatzConjecture.__getSequence(self.n)
+            self.sequence = self.__getSequence(self.n)
             end = time.time()
             self.elapsedTime = end - start
         # Case: 2 int param
         elif len(args) == 2 and type(args[0]) is type(args[1]) is int:
             start = time.time()
-            self.sequence = {x : CollatzConjecture.__getSequence(x) for x in range(args[0], args[1])}
+            self.sequence = {x : self.__getSequence(x) for x in range(args[0], args[1])}
             end = time.time()
             self.elapsedTime = end - start
         # Case: 1 list or tuple param
         elif len(args) ==1 and type(args[0]) is tuple or type(args[0]) is list:
             start = time.time()
-            self.sequence = {x : CollatzConjecture.__getSequence(x) for x in range(args[0][0], args[0][1])}
+            self.sequence = {x : self.__getSequence(x) for x in range(args[0][0], args[0][1])}
             end = time.time()
             self.elapsedTime = end - start
 
-    def __getSequence(n : int) -> list:
-        if n == 1:
+    def __getSequence(self, n : int) -> list:
+        """ if n == 1:
             return [1]
-        elif CollatzConjecture.__isPair(n):
-            return [n] + CollatzConjecture.__getSequence(int(n/2))
-        return [n] + CollatzConjecture.__getSequence(int(3*n+1))
+        elif CollatzConjecture_v2_DictOptimizated.__isPair(n):
+            return [n] + self.__getSequence(int(n/2))
+        return [n] + self.__getSequence(int(3*n+1)) """
+        """ if n == 1:
+            return [1]
+        el """
+        if n in self.optimization.keys():
+            return self.optimization[n]
+        elif CollatzConjecture_v2_DictOptimizated.__isPair(n):
+            self.optimization[n] = [n] + self.__getSequence(int(n/2))
+        else:
+            self.optimization[n] = [n] + self.__getSequence(int(3*n+1))
+        
+        return self.optimization[n]
     
     def __isPair(n : int) -> bool:
         if n % 2 == 0:
@@ -46,10 +59,3 @@ class CollatzConjecture:
 
     def fullDescription(self):
         return f"{self.__str__()}\nElapsed time = {self.elapsedTime}"
-
-""" # TEST BASE - DICT_OPTIMIZED
-A = CollatzConjecture(1, 100001)
-print(f"Tempo per la versione base = {A.elapsedTime}")
-B = CollatzConjecture_v2_DictOptimizated(1, 100001)
-print(f"Tempo per la versione dict_based = {B.elapsedTime}")
-print(f"Ottimizzazione = " + str(abs(round((A.elapsedTime-B.elapsedTime)/(A.elapsedTime+B.elapsedTime)*100, 3))) + "%") """
