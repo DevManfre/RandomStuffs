8d06a5cfe6718261cce0a5a42fe684d86fafaf60
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2023-03-08 12:19:31 +0100

    ✅ Add size test

--- Python/CollatzConjecture/VersionComparison.py
+++ Python/CollatzConjecture/VersionComparison.py
@@ -2,13 +2,17 @@ from CollatzConjecture import CollatzConjecture
 from CollatzConjectureDictOptimizated import CollatzConjectureDictOptimizated
 
 import os
+import sys
 
 def compareVersion():
-    def getPercentImprove(obj1 : CollatzConjecture, obj2 : CollatzConjecture):
+    def getPercentTimeImprove(obj1 : CollatzConjecture, obj2 : CollatzConjecture):
         value = (obj1.elapsedTime-obj2.elapsedTime)/(obj1.elapsedTime+obj2.elapsedTime)
         
         return abs(round(value * 100, 3))
     
+    def getPercentSpaceImprove(obj1 : CollatzConjecture, obj2 : CollatzConjecture):
+        return 0
+
     os.system("clear")
 
     start, end = 1, 100001
@@ -22,13 +26,23 @@ def compareVersion():
         print(f"\t{k} version \t\t {versions[k].elapsedTime}s")
     print()
 
+    print(f"Memory Occupation:")
+    for k in versions.keys():
+        if versions[k] == versions["Base"]:
+            ris = round(sys.getsizeof(versions[k])/1024/1024, 3)
+            print(f"\t{k} version\t\t{ris}Mb")
+        else:
+            print(f"{k} and base aren't equals...")
+    print()
+
     print(f"Optimization:")
     for k in versions.keys():
         if versions[k] == versions["Base"]:
-            optimization = getPercentImprove(versions["Base"], versions[k])
-            if int(optimization) == 0:
+            timeOptimization = getPercentTimeImprove(versions["Base"], versions[k])
+            spaceOptimization = getPercentSpaceImprove(versions["Base"], versions[k])
+            if int(timeOptimization) == 0:
                 continue
-            print(f"\t{k} - Base\t\t{optimization}%")
+            print(f"\t{k} - Base\t\tTime percent = {timeOptimization}%\t\tSpace percent = {spaceOptimization}%")
         else:
             print(f"{k} and base aren't equals...")
     print()
