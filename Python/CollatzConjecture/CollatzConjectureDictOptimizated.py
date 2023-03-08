ba97572e929c8e2ef2063286a4c6547b9942ca3f
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2023-03-08 11:53:14 +0100

    🎨 Inheritance improved

--- Python/CollatzConjecture/CollatzConjectureDictOptimizated.py
+++ Python/CollatzConjecture/CollatzConjectureDictOptimizated.py
@@ -3,27 +3,11 @@ from CollatzConjecture import CollatzConjecture
 
 class CollatzConjectureDictOptimizated(CollatzConjecture):
     def __init__ (self, *args):
-        self.optimization = {1:[1]}
-
-        # Case: 1 int param
-        if len(args) == 1 and type(args[0]) is int:
-            self.n = args[0]
-            start = time.time()
-            self.sequence = self._getSequence(self.n)
-            end = time.time()
-            self.elapsedTime = end - start
-        # Case: 2 int params
-        elif len(args) == 2 and type(args[0]) is type(args[1]) is int:
-            start = time.time()
-            self.sequence = {x : self._getSequence(x) for x in range(args[0], args[1])}
-            end = time.time()
-            self.elapsedTime = end - start
-        # Case: 1 list or tuple param
-        elif len(args) ==1 and type(args[0]) is tuple or type(args[0]) is list:
-            start = time.time()
-            self.sequence = {x : self._getSequence(x) for x in range(args[0][0], args[0][1])}
-            end = time.time()
-            self.elapsedTime = end - start
+        self.optimization = {
+            1:[1]
+        }
+        
+        super().__init__(args)
 
     def _getSequence(self, n : int) -> list:
         if n in self.optimization.keys():
