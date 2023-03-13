89afe690fbd55a6ac261049786e5f6061643fff2
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2023-03-13 18:38:01 +0100

    :hammer: Create the init method

--- Python/Keylogger/Keylogger.py
+++ Python/Keylogger/Keylogger.py
@@ -1,2 +1,9 @@
+import datetime
+
 class Keylogger:
-    pass
+    def __init__(self, interval=60, reportMethod="file"):
+        self.interval = interval
+        self.reportMethod = reportMethod
+        self.log = ""                           #contains the log of all the keystrokes within `self.interval`
+        self.startDatetime = datetime.now()
+        self.endDatetime = datetime.now()
