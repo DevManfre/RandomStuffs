931e4a29d173fd973ade7827d4364789617cb19b
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2023-04-28 15:28:11 +0200

    :hammer: Rewriting of some code

--- Python/InstagramFollowerCheck/InstragramBot.py
+++ Python/InstagramFollowerCheck/InstragramBot.py
@@ -15,37 +15,32 @@ class InstagramBot:
         self.token : str = self.api.generate_uuid()
         self.userId : str = self.api.authenticated_user_id
     
-    def followersInformation(self) -> dict:
+    def followersDict(self) -> dict:
         """
         Return a dict contains all information about every single followers.
         """
 
         return self.api.user_followers(self.userId, self.token)
     
-    def followingInformation(self) -> dict:
+    def followingsDict(self) -> dict:
         """
         Return a dict contains all information about every single following.
         """
+
         return self.api.user_following(self.userId, self.token)
     
-    def followUsernameSets(self) -> tuple:
-        followingSet : set = {user["username"] for user in self.followingInformation()["users"]}
-        followersSet : set = {user["username"] for user in self.followersInformation()["users"]}
-
-        return followingSet, followersSet
-    
     def unfollowersList(self) -> list:
-        followingSet, followersSet = self.followUsernameSets()
-        
-        unfollowersSet : set = followingSet.difference(followersSet)
-        unfollowersList : list = sorted(list(unfollowersSet))
+        followers : set = {user["username"] for user in self.followersDict()["users"]}
+        following : set = {user["username"] for user in self.followingsDict()["users"]}
 
-        return unfollowersList
+        unfollowers : list = sorted(list(following.difference(followers)))
+
+        return unfollowers
     
     def friendsList(self) -> list:
-        followingSet, followersSet = self.followUsernameSets()
+        followers : set = {user["username"] for user in self.followersDict()["users"]}
+        following : set = {user["username"] for user in self.followingsDict()["users"]}
 
-        friendsSet : set = followingSet.intersection(followersSet)
-        friendsList : set = sorted(list(friendsSet))
+        friends : list = sorted(list(following.intersection(followers)))
 
-        return friendsList
+        return friends
