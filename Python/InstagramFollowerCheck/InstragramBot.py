f452dc5f6b33e12354213ac0d80f8ebff46b2757
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2023-04-28 15:09:53 +0200

    :bulb: Add class comment

--- Python/InstagramFollowerCheck/InstragramBot.py
+++ Python/InstagramFollowerCheck/InstragramBot.py
@@ -1,6 +1,13 @@
 from instagram_private_api import Client
 
 class InstagramBot:
+    """
+    Implementantion for instagram_private_api
+    The init method accepts 2 params: username and password so it can have:
+    - api -> the logged client using the given username and password;
+    - token, userId -> used for many intragram_private_api function
+    """
+
     def __init__(self, username : str, password : str):
         self.username : str = username
         self.password : str = password
